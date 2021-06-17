cee6032e33973e46e57593ecfd574041bb8841b4
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-16 22:59:04 -0400

    clean

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import HeaderContainer from './Header/HeaderContainer'
+import HeaderContainer from './Header/HeaderContainer';
+import ForecastContainer from './Forecast/ForecastContainer';
 
 function App() {
   return (
     <div className="App">
       Hi
       <HeaderContainer />
+      <ForecastContainer />
     </div>
   );
 }
